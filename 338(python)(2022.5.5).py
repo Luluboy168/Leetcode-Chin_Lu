fd5def237785fc5fd71e14e86c4645b43681645d
hou ren hao <72810883+hourenhao1@users.noreply.github.com>
2022-05-05 12:00:41 +0800

    Create 338(python)(2022.5.5).py

---
+++ 338(python)(2022.5.5).py
@@ -0,0 +1,9 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        
+        ans = [0 for i in range(n + 1)]
+        for i in range(1, n + 1):
+            ans[i] = str(bin(i)).count('1')
+            
+        return ans
+        

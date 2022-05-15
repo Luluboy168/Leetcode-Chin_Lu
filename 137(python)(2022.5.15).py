9d0b1c292aa3fa3f84cfbb2f020bc141aed3beeb
hou ren hao <72810883+hourenhao1@users.noreply.github.com>
2022-05-15 16:54:05 +0800

    Update 137(python)(2022.5.15).py

--- 137(python)(2022.5.15).py
+++ 137(python)(2022.5.15).py
@@ -1,11 +1,18 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        
-        for num in nums:
-            if nums.count(num) == 1:
-                return num
-              
+        #set 會去掉重複元素而且排序
+        return int((3 * sum(set(nums)) - sum(nums)) * 0.5)
+    
+    
 # ##solution 2
+# class Solution:
+#     def singleNumber(self, nums: List[int]) -> int:
+        
+#         for num in nums:
+#             if nums.count(num) == 1:
+#                 return num
+              
+# ##solution 3
 # class Solution:
 #     def singleNumber(self, nums: List[int]) -> int:
         

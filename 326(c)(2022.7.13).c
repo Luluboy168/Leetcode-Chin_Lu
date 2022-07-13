0148c12e3855d9f077d2f28b78b7cb43de3696be
Chen Chin Lu <72810883+Luluboy168@users.noreply.github.com>
2022-07-13 11:43:05 +0800

    Update 326(c)(2022.7.13).c

--- 326(c)(2022.7.13).c
+++ 326(c)(2022.7.13).c
@@ -1,5 +1,6 @@
 bool isPowerOfThree(int n){ 
     //solution 1
+    // pow(3,19) (= 1,162,261,467) is the max int which is less than INT_MAX. 
     return (n > 0) && ((int)(pow(3,19)) % n == 0);
     
     //solution 2 
